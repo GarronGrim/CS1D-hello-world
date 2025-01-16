4f665384a56fe5c802cc7a98d79a81bd4d4e4dd4
k4teGr1m <katelgrim@gmail.com>
2025-01-15 21:44:06 -0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
-int hello1Function(string Garron);
+int hello1Function(string name1);
+int hello2Function(string name2);
 int main() {
   string name1 = "Garron Grim";
+  string name2 = "Kate Grim";
   hello1Function(name1);
+  hello2function(name2);
+  return 0; 
 }
